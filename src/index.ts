99411bb26480e3ba8e874a5f2b0da464079a84d5
Madhav <kmadhav824@gmail.com>
2026-02-05 20:02:45 +0530

    Ready to give file Paths for Object store

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,35 @@
 import express from 'express';
 import cors from 'cors';
+import { generate } from './utils.js';
+import { simpleGit } from 'simple-git';
+import { getAllFiles } from './file.js';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-app.listen(8080, () => {
+app.post('/deploy', async (req, res) => {
+  // Your deployment logic here
+  const repoUrl=req.body.repoUrl;
+  const id=generate();
+  console.log(`Deployment initiated for repository: ${repoUrl}`);
+  console.log(__dirname);
+  await simpleGit().clone(repoUrl, path.join(__dirname,`output/${id}`));
+  res.json({id});
+
+  const files = getAllFiles(path.join(__dirname,`output/${id}`));
+  console.log(files);
+});
+
+app.listen(3000, () => {
   console.log('Server is running on port 3000');
 })
 
