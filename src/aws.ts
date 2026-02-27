56c77080d73202fb53d83f6ed1f2d864b010719b
Kmadhav824 <kmadhav824@gmail.com>
2026-02-27 22:43:55 +0530

    Corrected imports, fixed logics , Fetching Clean

--- src/aws.ts
+++ src/aws.ts
@@ -1,11 +1,11 @@
-import dotenv from "dotenv";
-dotenv.config();
+import "dotenv/config";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { readFile } from "fs/promises";
 
 const s3 = new S3Client({
   region: "auto",
   endpoint: process.env.R2_API as string,
+  forcePathStyle: true,
   credentials: {
     accessKeyId: process.env.R2_ACCESS_KEY_ID as string,
     secretAccessKey: process.env.R2_SECRET_ACCESS_KEY as string
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from 'express';
 import cors from 'cors';
 import { generate } from './utils.js';
@@ -14,6 +15,9 @@ import { createClient } from "redis";
 const publisher = createClient();
 publisher.connect();
 
+const subscriber = createClient();
+subscriber.connect();
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -24,28 +28,38 @@ app.use(express.json());
 
 app.post('/store', async (req, res) => {
   // Deployment logic
-  const repoUrl=req.body.repoUrl;
-  const id=generate();
+  const repoUrl = req.body.repoUrl;
+  const id = generate();
   console.log(`Deployment initiated for repository: ${repoUrl}`);
   console.log(__dirname);
-  await simpleGit().clone(repoUrl, path.join(__dirname,`output/${id}`));
-  res.json({id});
+  await simpleGit().clone(repoUrl, path.join(__dirname, `output/${id}`));
 
-  const files = getAllFiles(path.join(__dirname,`output/${id}`));
+  const files = getAllFiles(path.join(__dirname, `output/${id}`));
   for (const file of files) {
-      const relativePath = path.relative(path.join(__dirname, `output/${id}`), file);
-      const key = `${id}/${relativePath}`;
-      
-      // We skip the .git folder to save time and bandwidth
-      if (relativePath.includes('.git')) continue;
+    const relativePath = path.relative(path.join(__dirname, `output/${id}`), file);
+    const key = `${id}/${relativePath}`;
+    console.log(key);
+    // We skip the .git folder to save time and bandwidth
+    if (relativePath.includes('.git')) continue;
 
-      await uploadFile(key, file);
+    await uploadFile(key, file);
   }
 
   // After all files are done, you can then tell Redis
   publisher.lPush("build-queue", id);
-    console.log(files);
-  });
+  console.log("files uploaded");
+  publisher.hSet("status", id, "uploaded");
+
+  res.json({ id: id });
+});
+
+app.get("/status", async (req, res) => {
+  const id = req.query.id;
+  const response = await subscriber.hGet("status", id as string);
+  res.json({
+    status: response
+  })
+})
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
